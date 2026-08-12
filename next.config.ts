6787a8c7280018344140cabfb27eaa9db380937b
obuhdaniel <danielobuh05@gmail.com>
2026-08-12 16:16:05 +0100

    fix: breaking images

--- next.config.ts
+++ next.config.ts
@@ -49,14 +49,6 @@ const nextConfig: NextConfig = {
             key: 'X-Domain',
             value: baseDomain,
           },
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
         ],
       },
        {
